af7399352dcb8ca8f80633c21f76c9ed994ca3c3
DanTran <dantran@DanTrans-MacBook-Pro.local>
2023-07-02 21:05:43 +0700

    1. AOP (Aspect-Oriented Programming)

--- spring-boot/springmvc/src/main/java/edu/kits/springmvc/Controller/UserController.java
+++ spring-boot/springmvc/src/main/java/edu/kits/springmvc/Controller/UserController.java
@@ -16,16 +16,29 @@ public class UserController {
     UserService userService;
     @GetMapping
     public String getAllUsers(Model model) {
+        System.out.println("Exercuting Controller - getAllUsers");
         List<User> users = userService.getAllUsers();
         model.addAttribute("users", users);
         return "userList";
     }
 
+    @GetMapping("/json")
+    @ResponseBody
+    public List<User> getAllUsersJson(Model model) {
+        System.out.println("Exercuting Controller - getAllUsers");
+        List<User> users = userService.getAllUsers();
+        model.addAttribute("users", users);
+        return users;
+    }
+
     @GetMapping("/{id}")
     public String getUserById(@PathVariable Long id, Model model) {
+        System.out.println("Exercuting Controller - getUserById");
         User user = userService.getUserById(id);
         model.addAttribute("user", user);
         return "userDetails";
     }
+
+
 // Other controller methods for creating, updating, and deleting users
 }
